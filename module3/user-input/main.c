5cf5615f101d5ec671e17d445ddd1ccbad4e7ec6
andrekeys <andrekeys@boisestate.edu>
2024-09-03 13:07:41 -0600

    scanf example

---
+++ module3/user-input/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int myint;
+    printf("Please enter an integer\n");
+    scanf("%d", &myint);
+
+    printf("myint value is now %d\n", myint);
+
+    return 0;
+}
