57532112dcebeec482264a3ee54eef4521a283c9
andrekeys <andrekeys@boisestate.edu>
2024-09-10 12:04:38 -0600

    updating module 4 error example

--- module04/errors/main.c
+++ module04/errors/main.c
@@ -1,32 +1,30 @@
-#include<stdio.h>
-#include <string.h>
+#include <stdio.h>
+
+#undef FLAG
 
 int main(void) {
-   char serviceWord1[50];
-   char serviceWord2[50];
    
-   // Prompt user for desired auto service, grab both strings
-   printf("Enter desired auto service:\n");
-   scanf("%s", serviceWord1);
-   scanf("%s", serviceWord2);
-   
-   printf("You entered: %s %s\n", serviceWord1, serviceWord2);
-   
-   if ((strcmp(serviceWord1, "Oil") == 0) && (strcmp(serviceWord2, "change") == 0)) {
-      printf("Cost of oil change: $35\n");
+   #ifdef FLAG
+   //fprintf allows us to select an output channel (stdout, stderr)
+
+   fprintf(stdout, "stdout: Hello world!\n");
+
+   fprintf(stderr, "ERROR: something wrong\n");
+
+   #else
+   printf("Please enter a double\n");
+
+   double number;
+
+   int returnCode = scanf("%lf", &number);
+
+   if (returnCode < 1) {
+      fprintf(stderr, "Error: Incorrect format\n");
+   } else {
+      printf("scanned %lf\n", number);
    }
-   
-   else if ((strcmp(serviceWord1, "Tire") == 0) && (strcmp(serviceWord2, "rotation") == 0)) {
-      printf("Cost of tire rotation: $19\n");
-   }
-   
-   else if ((strcmp(serviceWord1, "Car") == 0) && (strcmp(serviceWord2, "wash") == 0)) {
-      printf("Cost of car wash: $7\n");
-   }
-   
-   else {
-      printf("Error: Requested service is not recognized\n");
-   }
-   
+
+   #endif
+
    return 0;
 }
