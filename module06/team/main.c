90970a9186e45b6b49d0e339a9ac8feed853e433
Andre Keys <andrekeys@boisestate.edu>
2024-09-26 13:00:37 -0600

    updating

--- module06/team/main.c
+++ module06/team/main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 
+#define TEAM_SIZE 5
+
 int main(void) {
 
-   /* Type your code here. */
+    // declare data. Need an array for storage, some data for total and max height
 
-   return 0;
+    /* Read Heights and store in array */
+
+    /* Display Heights on a single line */
+    printf("You entered: ");
+    for (int i = 0; i < TEAM_SIZE; i++) {
+    }
+    printf("\n\n");
+
+    /* Total Height */
+    for (int i = 0; i < TEAM_SIZE; i++) {
+    }
+
+    /* Calculate maxHeight */
+    for (int i = 0; i < TEAM_SIZE; i++) {
+    }
+    /* Print data */
+    printf("Total Height: %0.2lf\n", totalHeight);
+    printf("Average Height: %0.2lf\n", totalHeight/TEAM_SIZE);
+    printf("Max Height: %0.2lf\n", maxHeight);
+
+    return 0;
 }
