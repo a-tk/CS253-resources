6cd9e180820bd7319ba56e7fb2df6598ce647872
Andre Keys <andrekeys@boisestate.edu>
2024-10-31 13:11:12 -0600

    updating examples

---
+++ module07/input/text-assistant/text-assistant.c
@@ -0,0 +1,142 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#define MAX_LINE 256
+
+int GetNumOfNonWSCharacters(const char usrStr[]) {
+    int count;
+    unsigned i;
+
+    count = 0;
+
+    for (i = 0; i < strlen(usrStr); ++i) {
+        if (usrStr[i] != ' ' && usrStr[i] != '\t' && usrStr[i] != '\0'
+            && usrStr[i] != '\n') {
+            ++count;
+        }
+    }
+
+    return count;
+}
+
+int GetNumOfWords(const char usrStr[]) {
+    int count;
+    unsigned i;
+
+    count = 0;
+
+    for (i = 1; i < strlen(usrStr); ++i) {
+        if (isspace(usrStr[i]) && !isspace(usrStr[i - 1])) {
+            ++count;
+        }
+    }
+
+    if (!isspace(usrStr[strlen(usrStr) - 1])) {
+        ++count;
+    }
+
+    return count;
+}
+
+void ShortenSpace(char usrStr[]) {
+    unsigned i;
+    char tmp[256] = "";
+    int length;
+
+
+    for (i = 1; i < strlen(usrStr); ++i) {
+        if (!(isspace(usrStr[i]) && isspace(usrStr[i - 1]))) {
+            length = strlen(tmp);
+            tmp[length] = usrStr[i - 1];
+            tmp[length + 1] = '\0';
+        }
+
+        if (i == strlen(usrStr) - 1) {
+            length = strlen(tmp);
+            tmp[length] = usrStr[i];
+            tmp[length + 1] = '\0';
+        }
+    }
+
+    strcpy(usrStr,tmp);
+}
+
+
+
+// get words, get nonWS chars, shorten spaces, and fix capitals
+void printUsage(const char *programName) {
+    fprintf(stdout,"Usage: %s [-w] [-c] [-s] [-f <filename>] [-h]\n", programName);
+    fprintf(stdout,"\t-w            print number of words\n");
+    fprintf(stdout,"\t-c            print number of non-whitespace characters\n");
+    fprintf(stdout,"\t-s            shorten spaces\n");
+    fprintf(stdout,"\t-f <filename> REQUIRED: Filename to read text from\n");
+    fprintf(stdout,"\t-h            print this help\n");
+}
+
+int main(int argc, char **argv) {
+
+    int opt;
+    bool countWords = false;
+    bool countCharacters = false;
+    bool shortenSpaces = false;
+    bool providedFile = false;
+    char *filename;
+
+
+    char *line;
+    int lineSize;
+    char c;
+
+    while ( (opt = getopt(argc, argv, "")) != -1 ) {
+        switch (opt) {
+            case 'h':
+                printUsage(argv[0]);
+                exit(0);
+            default:
+                printUsage(argv[0]);
+                exit(1);
+        }
+    }
+
+    if (providedFile == false) {
+        printUsage(argv[0]);
+        exit(-1);
+    }
+
+    FILE *f = fopen(filename, "r"); //open the file in read mode
+    if (f == NULL) {
+        perror("fopen");
+        exit(1);
+    }
+
+    //while there are lines
+    while ((c = fgetc(f)) != EOF) {
+        
+        lineSize = 0;
+        line = malloc(sizeof(char) * MAX_LINE);
+
+        //read the line in, filling the line buffer up untile MAX_LINE
+        while (((c != '\n') && (c != EOF)) && (lineSize < MAX_LINE - 1)) {
+            line[lineSize] = c;
+            lineSize++;
+            c = fgetc(f);
+        }
+        
+        //ensure that the line string is null terminated
+        line[lineSize] = '\0';
+
+        //TODO
+
+        // manipulate the line
+        // print the line
+
+        free(line);
+    }
+
+    fclose(f);
+}
