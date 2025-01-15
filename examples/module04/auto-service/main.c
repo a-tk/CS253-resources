8c5d4766c8c37e272f8cf644187d9e7994c5f82f
andrekeys <andrekeys@boisestate.edu>
2025-01-15 12:56:43 -0700

    removing old

--- examples/module04/auto-service/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-// FIXME include the string library
-
-int main(void) {
-
-    /* Type your code here. */
-
-    return 0;
-}
--- examples/module07/SinglyLinkedList/common.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef __COMMON_H
-#define __COMMON_H
-
-#include <string.h>
-#include <stdbool.h>
-
-#endif /* __COMMON_H */
