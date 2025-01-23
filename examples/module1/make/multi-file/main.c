37c11f732c5cd27c1725a42c4ae26c55929daf2c
andrekeys <andrekeys@boisestate.edu>
2025-01-23 11:30:54 -0700

    updating

--- examples/module1/make/multi-file/main.c
+++ examples/module1/make/multi-file/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "square.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <number>", argv[0]);
+        return 0;
+    }
+
+    int arg = atoi(argv[1]);
 
     printf("the square of 4 is %d\n", square(4));
 
