4c2070046866d5b4b0260873135b25f003678325
andrekeys <andrekeys@boisestate.edu>
2025-03-11 16:04:43 -0600

    adding test

---
+++ examples/module3/DoublyLinkedList/simple-test.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "List.h"
+
+
+int main(int argc, char **argv) {
+	int i;
+	int n;
+	ListPtr list;
+
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <list size> \n", argv[0]);
+		exit(1);
+	}
+	n = atoi(argv[1]);
+
+	list = CreateList();
+	for (i = 0; i < n; i++) {
+		ListAddAtFront(list, (n-i));
+	}
+
+	ListPrint(list);
+    ListReverse(list);
+	ListPrint(list);
+
+	DestroyList(list);
+
+	exit(0);
+}
