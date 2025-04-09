583003ed157ddca5ad68ad56fb25fe947ea9916f
Andre Keys <andrekeys@boisestate.edu>
2025-04-09 10:46:44 -0600

    using a shared object

--- examples/module4/liblist/SimpleTest.c
+++ examples/module4/liblist/SimpleTest.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "SinglyLinkedList.h"
-#include "Job.h"
+#include <SinglyLinkedList.h>
 
+#include "Job.h"
 
 int main(int argc, char **argv) {	
 	int i;
