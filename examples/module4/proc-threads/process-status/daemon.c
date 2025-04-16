7ab05b0c96be3ecdbb5c212fe6d38dcfac2f4194
andrekeys <andrekeys@boisestate.edu>
2025-04-16 13:42:37 -0600

    actually a daemon now

--- examples/module4/proc-threads/process-status/daemon.c
+++ examples/module4/proc-threads/process-status/daemon.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <fcntl.h>
 
+static int fd;
 
 /**
- * Spawns a child that is never waited for by the parent.
- * The child is never reaped, therefore leaving it a zombie
+ * Spawns a child that uses the daemon pattern to read random numbers
+ * from the system random number generator
  */
 int main(void) {
     int pid = fork();
@@ -14,14 +16,51 @@ int main(void) {
     if (pid < 0) {
         perror("fork");
         exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-        // child
-
+    } else if (pid != 0) {
+        //parent
         exit(EXIT_SUCCESS);
     } else {
-        //parent
-        while (1) {
-            sleep(60);
+        // child
+        pid = fork();
+
+        
+        if (pid < 0) {
+            perror("second fork");
+            exit(EXIT_FAILURE);
+        } else if (pid != 0) {
+            //parent
+            exit(EXIT_SUCCESS);
+        } else {
+
+            if (setsid() < 0) {
+                perror("setsid");
+                exit(EXIT_FAILURE);
+            }
+
+            if (chdir("/") < 0) {
+                perror("chdir");
+                exit(EXIT_FAILURE);
+            }
+
+            fd = open("/dev/random", O_RDONLY);
+            if (fd < 0) {
+                perror("open");
+                exit(EXIT_FAILURE);
+            }
+
+            while(1) {
+                int random_value;
+                ssize_t result = read(fd, &random_value, sizeof(int));
+                if (result < 0) {
+                    perror("read");
+                    close(fd);
+                    exit(EXIT_FAILURE);
+                }
+
+                printf("daemon random number was %d\n", random_value);
+
+                sleep(10);
+            }
         }
     }
 }
